0057c5e78460047efb13417dfd3beb1f627f65b6
Debra Gordon <debra.m.gordon@gmail.com>
2017-06-05 15:31:52 -0700

    connecting models to the database

---
+++ ChickChick/DAL/AppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ChickChick.Models;
+
+namespace ChickChick.DAL
+{
+    public class AppContext : DbContext
+    {
+        public AppContext() : base("ChickChickApp") { }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<WaitingStudent> WaitingStudents { get; set; }
+
+    }
+}
